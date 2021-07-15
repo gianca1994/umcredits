f6e54e304917fd271db580b57eb886f192ad3436
Giancarlo <g.galvarini@alumno.um.edu.ar>
2021-07-15 00:53:21 -0300

    initialized-project

---
+++ umcredits/src/main/java/com/gianca1994/umcredits/UmcreditsApplication.java
@@ -0,0 +1,13 @@
+package com.gianca1994.umcredits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcreditsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcreditsApplication.class, args);
+	}
+
+}
---
+++ umcredits/src/test/java/com/gianca1994/umcredits/UmcreditsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianca1994.umcredits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcreditsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
